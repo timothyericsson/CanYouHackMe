b36168bd2545afa68c3fce4dc2b55713ddf957d0
timothyericsson <tim@oscpmentor.com>
2023-05-25 21:03:09 +0700

    0.3 small updates

--- WebFiles/login.php
+++ WebFiles/login.php
@@ -7,50 +7,89 @@ $db = mysqli_connect("localhost", "root", "backpack10", "users");
 
 // check if the form is submitted
 if (isset($_POST["login"])) {
-  // get the form data
-  $username = $_POST["username"];
-  $password = $_POST["password"];
+ // get the form data
+ $username = $_POST["username"];
+ $password = $_POST["password"];
 
-  // validate the form data
-  if (empty($username) || empty($password)) {
-    // display an error message
-    echo "Please enter your username and password.";
-  } else {
-    // check if the user exists in the database
-    $sql = "SELECT * FROM security WHERE username = '$username' AND password = '$password'";
-    $result = mysqli_query($db, $sql);
-    if (mysqli_num_rows($result) == 1) {
-      // set the session variable
-      $_SESSION["username"] = $username;
-      // redirect to the home page
-      header("Location: index.php");
-    } else {
-      // display an error message
-      echo "Invalid username or password.";
-    }
-  }
+ // validate the form data
+ if (empty($username) || empty($password)) {
+ // display an error message
+ echo "Please enter your username and password.";
+ } else {
+ // check if the user exists in the database
+ $sql = "SELECT * FROM security WHERE username = '$username' AND password = '$password'";
+ $result = mysqli_query($db, $sql);
+ if (mysqli_num_rows($result) == 1) {
+ // set the session variable
+ $_SESSION["username"] = $username;
+ // redirect to the home page
+ header("Location: index.php");
+ } else {
+ // display an error message
+ echo "Invalid username or password.";
+ }
+ }
 }
-// Create a container div for the buttons and set its position, width and height
-// Adjust width +100px per extra button added
-echo "<div style='position: absolute; top: 0; right: 0; width: 450px; height: 100px;'>";
-
-// Create the buttons and set their margin and onclick attributes
-echo "<button style='margin: 10px;' onclick='location.href=\"index.php\"'>Home</button>";
-echo "<button style='margin: 10px;' onclick='location.href=\"register.php\"'>Register</button>";
-echo "</div>";
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Login Page</title>
+ <title>Login</title>
+ <!-- Add some CSS to style the page -->
+ <style>
+ /* Set the background color and font family for the body */
+ body {
+  background-color: lightblue;
+  font-family: Verdana;
+ }
+ /* Set the color and font family for the h1 */
+ h1 {
+  color: white;
+  font-family: Arial;
+ }
+ /* Set the position, width and height for the container div */
+ .container {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 500px;
+  height: 100px;
+ }
+ /* Set the margin and onclick attributes for the buttons */
+ .button {
+  margin: 10px;
+ }
+ /* Set the position, width, height, margin, border and padding for the box div */
+ .box {
+  position: absolute;
+  width: 400px;
+  height: 200px;
+  margin-left: -200px;
+  margin-top: -150px;
+  left: 50%;
+  top: 50%;
+  border: 2px solid white;
+  padding: 20px;
+ }
+ </style>
 </head>
 <body>
-  <h1>Login Page</h1>
-  <form method="post" action="login.php">
-    <p>Username: <input type="text" name="username"></p>
-    <p>Password: <input type="password" name="password"></p>
-    <p><input type="submit" name="login" value="Login"></p>
-  </form>
+ <div class="container">
+ <!-- Create the buttons and set their onclick attributes -->
+ <button class="button" onclick='location.href="index.php"'>Home</button>
+ <button class="button" onclick='location.href="register.php"'>Register</button>
+ </div>
+ <div class="box">
+ <!-- Display the title -->
+ <h1>Login</h1>
+ <!-- Create the form and set its method and action attributes -->
+ <form method="post" action="login.php">
+ <!-- Create the input fields and labels -->
+ <p>Username: <input type="text" name="username"></p>
+ <p>Password: <input type="password" name="password"></p>
+ <!-- Create the submit button -->
+ <p><input type="submit" name="login" value="Login"></p>
+ </form>
+ </div>
 </body>
 </html>
--- WebFiles/register.php
+++ WebFiles/register.php
@@ -4,53 +4,92 @@ $db = mysqli_connect("localhost", "root", "backpack10", "users");
 
 // check if the form is submitted
 if (isset($_POST["register"])) {
-  // get the form data
-  $username = $_POST["username"];
-  $password = $_POST["password"];
+ // get the form data
+ $username = $_POST["username"];
+ $password = $_POST["password"];
 
-  // validate the form data
-  if (empty($username) || empty($password)) {
-    // display an error message
-    echo "Please fill in all the fields.";
-  } else {
-    // check if the username or email already exists
-    $sql = "SELECT * FROM security WHERE username = '$username'";
-    $result = mysqli_query($db, $sql);
-    if (mysqli_num_rows($result) > 0) {
-      // display an error message
-      echo "The username or email is already taken.";
-    } else {
-      // insert the user into the database
-      $sql = "INSERT INTO security (username, password) VALUES ('$username', '$password')";
-      mysqli_query($db, $sql);
-      // display a success message
-      echo "You have registered successfully.";
-      // redirect the user to the login page after three seconds
-      header("Refresh: 2; URL=login.php");
-    }
-  }
+ // validate the form data
+ if (empty($username) || empty($password)) {
+ // display an error message
+ echo "Please fill in all the fields.";
+ } else {
+ // check if the username or email already exists
+ $sql = "SELECT * FROM security WHERE username = '$username'";
+ $result = mysqli_query($db, $sql);
+ if (mysqli_num_rows($result) > 0) {
+ // display an error message
+ echo "The username or email is already taken.";
+ } else {
+ // insert the user into the database
+ $sql = "INSERT INTO security (username, password) VALUES ('$username', '$password')";
+ mysqli_query($db, $sql);
+ // display a success message
+ echo "You have registered successfully.";
+ // redirect the user to the login page after three seconds
+ header("Refresh: 2; URL=login.php");
+ }
+ }
 }
-// Create a container div for the buttons and set its position, width and height
-// Adjust width +100px per extra button added
-echo "<div style='position: absolute; top: 0; right: 0; width: 450px; height: 100px;'>";
-
-// Create the buttons and set their margin and onclick attributes
-echo "<button style='margin: 10px;' onclick='location.href=\"index.php\"'>Home</button>";
-echo "<button style='margin: 10px;' onclick='location.href=\"login.php\"'>Login</button>";
-echo "</div>";
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Register Page</title>
+ <title>Register</title>
+ <!-- Add some CSS to style the page -->
+ <style>
+ /* Set the background color and font family for the body */
+ body {
+  background-color: lightblue;
+  font-family: Verdana;
+ }
+ /* Set the color and font family for the h1 */
+ h1 {
+  color: white;
+  font-family: Arial;
+ }
+ /* Set the position, width and height for the container div */
+ .container {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 500px;
+  height: 100px;
+ }
+ /* Set the margin and onclick attributes for the buttons */
+ .button {
+  margin: 10px;
+ }
+ /* Set the position, width, height, margin, border and padding for the box div */
+ .box {
+  position: absolute;
+  width: 400px;
+  height: 200px;
+  margin-left: -200px;
+  margin-top: -150px;
+  left: 50%;
+  top: 50%;
+  border: 2px solid white;
+  padding: 20px;
+ }
+ </style>
 </head>
 <body>
-  <h1>Register Page</h1>
-  <form method="post" action="register.php">
-    <p>Username: <input type="text" name="username"></p>
-    <p>Password: <input type="password" name="password"></p>
-    <p><input type="submit" name="register" value="Register"></p>
-  </form>
+ <div class="container">
+ <!-- Create the buttons and set their onclick attributes -->
+ <button class="button" onclick='location.href="index.php"'>Home</button>
+ <button class="button" onclick='location.href="login.php"'>Login</button>
+ </div>
+ <div class="box">
+ <!-- Display the title -->
+ <h1>Register</h1>
+ <!-- Create the form and set its method and action attributes -->
+ <form method="post" action="register.php">
+ <!-- Create the input fields and labels -->
+ <p>Username: <input type="text" name="username"></p>
+ <p>Password: <input type="password" name="password"></p>
+ <!-- Create the submit button -->
+ <p><input type="submit" name="register" value="Register"></p>
+ </form>
+ </div>
 </body>
 </html>
